a404546cafc33b28642c8c4b8fe270091c4e32f4
Dat Do <bdatdo0601@dats-mbp.myfiosgateway.com>
2021-12-18 21:53:56 -0500

    docs: update some documentation

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -14,9 +14,11 @@ const Home = () => {
         🏹 Project Chiron
       </h1>
       <h2 className="text-4xl my-4 z-10 text-white">
-        Just another simple platform for your portfolio
+        Just another simple platform to build your portfolio
       </h2>
-      <h4 className="text-2xl my-2 z-10 text-white">Coming Soon...</h4>
+      <h4 className="text-2xl my-2 z-10 text-white">Build with ❤️ and collaborative efforts from your favorite communites</h4>
+      <h5 className="text-xl my-1 z-10 text-white">Coming Soon...</h5>
+      <h6 className="text-lg my-2 z-10 underline text-blue-400"><a href="https://datdo.notion.site/Proposal-Project-Chiron-4adda27de97d40e18987350b554b8fbb">Checkout our Vision</a></h6>
     </div>
   );
 };
