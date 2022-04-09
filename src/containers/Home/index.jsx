ac9e6aed865128f2a6a05faee7fb090282af85d2
Dat Do <bdatdo0601@dats-mbp.myfiosgateway.com>
2022-04-08 21:24:48 -0400

    fix: update background

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,24 +1,16 @@
 import React from "react";
-import Particles from "react-tsparticles";
 
 import "./index.css";
-import ParticlesConfig from "./ParticlesConfig";
 
 const Home = () => {
   return (
-    <div className="h-screen w-screen p-5 text-center grid content-center grid-cols-1 relative">
-      <Particles
-        className="particles fixed h-full w-full bg-blue-300"
-        options={ParticlesConfig}
-        style={{ zIndex: -9999 }}
-      />
-      <h1 className="font-bold text-8xl my-5 text-white">
-        Project Chiron 🏹 
-      </h1>
+    <div className="h-screen w-screen p-5 text-center grid content-center grid-cols-1 relative bg-blue-400">
+      <div className="bg fixed h-full w-full " />
+      <h1 className="font-bold text-8xl my-5 text-white">Project Chiron 🏹</h1>
       <h2 className="text-4xl my-4 text-white">
-        A different kind of learning platform
+        Some kind of learning platform?
       </h2>
-      <h6 className="text-xl my-2 underline"><a className="text-blue-400" href="https://datdo.notion.site/Proposal-Project-Chiron-4adda27de97d40e18987350b554b8fbb">Coming Soon... ?</a></h6>
+      <h6 className="text-xl my-2 underline">Coming Soon...</h6>
     </div>
   );
 };
